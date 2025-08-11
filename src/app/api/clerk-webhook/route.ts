13d5eef3d64ad1d167620d18087cf471603681f1
ishanjarwal <ishan.professional01@gmail.com>
2025-08-11 19:33:43 +0530

    add logs to clerk webhook

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -3,43 +3,58 @@ import { headers } from "next/headers";
 import prisma from "@/config/prisma";
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-  if (!WEBHOOK_SECRET) {
-    throw new Error("Missing CLERK_WEBHOOK_SECRET env variable");
-  }
-
-  // Verify the request came from Clerk
-  const payload = await req.text();
-  const headers = req.headers as unknown as WebhookRequiredHeaders;
-  const wh = new Webhook(WEBHOOK_SECRET);
-
-  let evt;
   try {
-    evt = wh.verify(payload, headers) as any;
-  } catch (err) {
-    return new Response("Invalid signature", { status: 400 });
-  }
-  console.log("Event Received : ", evt);
-  const { id, type, data } = evt;
+    const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+    if (!WEBHOOK_SECRET) {
+      throw new Error("Missing CLERK_WEBHOOK_SECRET env variable");
+    }
 
-  if (type === "user.created" || type === "user.updated") {
-    await prisma.user.upsert({
-      where: { clerkId: data.id },
-      update: {
-        name: data.first_name,
-        email: data.email_addresses[0].email_address,
-      },
-      create: {
-        clerkId: data.id,
-        name: data.first_name,
-        email: data.email_addresses[0].email_address,
-      },
-    });
-  }
+    // Verify the request came from Clerk
+    const payload = await req.text();
+    const headers = req.headers as unknown as WebhookRequiredHeaders;
 
-  if (type === "user.deleted") {
-    await prisma.user.delete({ where: { clerkId: data.id } });
-  }
+    console.log("Payload and Headers", payload, headers);
 
-  return new Response("Webhook received", { status: 200 });
+    const wh = new Webhook(WEBHOOK_SECRET);
+
+    console.log("Wh Webhook : ", wh);
+
+    let evt;
+    try {
+      evt = wh.verify(payload, headers) as any;
+      console.log("Event Verified");
+    } catch (err) {
+      console.log("Event Verification Error : ", err);
+      return new Response("Invalid signature", { status: 400 });
+    }
+
+    console.log("Event Received : ", evt);
+
+    const { id, type, data } = evt;
+
+    if (type === "user.created" || type === "user.updated") {
+      console.log("Now Creating user or upserting");
+
+      await prisma.user.upsert({
+        where: { clerkId: data.id },
+        update: {
+          name: data.first_name,
+          email: data.email_addresses[0].email_address,
+        },
+        create: {
+          clerkId: data.id,
+          name: data.first_name,
+          email: data.email_addresses[0].email_address,
+        },
+      });
+    }
+
+    if (type === "user.deleted") {
+      await prisma.user.delete({ where: { clerkId: data.id } });
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (error) {
+    return new Response(`Something went wrong : ${error}`, { status: 500 });
+  }
 }
