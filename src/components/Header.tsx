a8d580117b559cdd0f70e43317f7ed869e7681b6
ishanjarwal <ishan.professional01@gmail.com>
2025-08-11 18:02:04 +0530

    new navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,89 +4,170 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Plus } from "lucide-react";
-const Header = () => {
-  const navbar = [
-    { name: "Home", link: "/" },
-    { name: "About", link: "/about" },
-    { name: "features", link: "/" },
-  ];
 
+import { Book, Menu, Sunset, Trees, Zap } from "lucide-react";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+//   Sheet,
+//   SheetContent,
+//   SheetHeader,
+//   SheetTitle,
+//   SheetTrigger,
+// } from "@/components/ui/sheet";
+
+const logo = {
+  url: "/",
+  src: "/logo_white.png",
+  alt: "logo",
+  title: "TripSage",
+};
+const navbar = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "/about" },
+  { name: "features", link: "/" },
+];
+const auth = {
+  login: { title: "Login", url: "/sign-in" },
+  signup: { title: "Sign up", url: "sign-up" },
+};
+
+const Header = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <motion.header
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1.4, delay: 0.5 }}
-      className="w-full text-white  border-b  py-4 z-50 px-14 justify-between  fixed    left-1/2 -translate-x-1/2 bg-black/10 border border-white/20 backdrop-blur-lg shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] flex items-center gap-x-10  top-0"
-    >
-      <a href="" className="logo text-2xl">
-        Odoo
-      </a>
-      <nav className="absolute left-1/2 -translate-x-1/2 flex items-center gap-x-10 px-10 py-3 rounded-2xl">
-        <Link
-          href={"/"}
-          onClick={() => {
-            scrollTo(0, 0);
-          }}
-        >
-          Home
-        </Link>
-        <Link
-          href={"/about"}
-          onClick={() => {
-            scrollTo(0, 0);
-          }}
-        >
-          About
-        </Link>
-        <Link
-          href={"/"}
-          onClick={() => {
-            const features = document.querySelector("#features");
-            features?.scrollIntoView();
-          }}
-        >
-          Features
-        </Link>
-      </nav>
-      <div className="login-signup flex items-center gap-x-10">
-        {isSignedIn ? (
-          <div className="flex justify-end items-center space-x-4">
-            <Button asChild>
-              <Link href={"/create"}>
-                <span>Create a New Trip</span>
-                <Plus />
-              </Link>
-            </Button>
-            <UserButton
-              appearance={{
-                elements: {
-                  userButtonAvatarBox: {
-                    width: "48px",
-                    height: "48px",
-                  },
-                },
-              }}
-            />
+    <section className="py-4 px-4 sticky top-0 backdrop-blur-sm bg-black/80 z-[100000000] shadow-[0px_4px_22px_3px_var(--color-primary)]">
+      <div className="container mx-auto">
+        {/* Desktop Menu */}
+        <nav className="hidden justify-between lg:flex">
+          <div className="flex items-center gap-6">
+            {/* Logo */}
+            <a href={logo.url} className="flex items-center gap-2">
+              <img
+                src={logo.src}
+                className="max-h-8 dark:invert"
+                alt={logo.alt}
+              />
+            </a>
+            <div className="flex items-center space-x-2">
+              {navbar.map((el: any, index: number) => (
+                <Link className="text-accent" href={el.link}>
+                  {el.name}
+                </Link>
+              ))}
+            </div>
           </div>
-        ) : (
-          <>
-            {" "}
-            <Link href={"/sign-in"} className="">
-              Login
-            </Link>
-            <Link
-              href={"/sign-up"}
-              className="px-4 py-1 rounded-lg bg-stone-800/90"
-            >
-              Sign Up
-            </Link>
-          </>
-        )}
+          <div className="flex gap-2">
+            {isSignedIn ? (
+              <div className="flex justify-end items-center space-x-4">
+                <Button asChild>
+                  <Link href={"/create"}>
+                    <span>Create a New Trip</span>
+                    <Plus />
+                  </Link>
+                </Button>
+                <UserButton
+                  appearance={{
+                    elements: {
+                      userButtonAvatarBox: {
+                        width: "48px",
+                        height: "48px",
+                      },
+                    },
+                  }}
+                />
+              </div>
+            ) : (
+              <>
+                <Button asChild variant="outline" size="sm">
+                  <a href={auth.login.url}>{auth.login.title}</a>
+                </Button>
+                <Button asChild size="sm">
+                  <a href={auth.signup.url}>{auth.signup.title}</a>
+                </Button>
+              </>
+            )}
+          </div>
+        </nav>
+
+        {/* Mobile Menu */}
       </div>
-    </motion.header>
+    </section>
   );
 };
 
 export default Header;
+
+// const MobileMenu = () => {
+//   return (
+//     <div className="block lg:hidden">
+//           <div className="flex items-center justify-between">
+//             {/* Logo */}
+//             <a href={logo.url} className="flex items-center gap-2">
+//               <img
+//                 src={logo.src}
+//                 className="max-h-8 dark:invert"
+//                 alt={logo.alt}
+//               />
+//             </a>
+//             <Sheet>
+//               <SheetTrigger asChild>
+//                 <Button variant="outline" size="icon">
+//                   <Menu className="size-4" />
+//                 </Button>
+//               </SheetTrigger>
+//               <SheetContent className="overflow-y-auto">
+//                 <SheetHeader>
+//                   <SheetTitle>
+//                     <a href={logo.url} className="flex items-center gap-2">
+//                       <img
+//                         src={logo.src}
+//                         className="max-h-8 dark:invert"
+//                         alt={logo.alt}
+//                       />
+//                     </a>
+//                   </SheetTitle>
+//                 </SheetHeader>
+//                 <div className="flex flex-col gap-6 p-4">
+//                   <div className="flex flex-col gap-3">
+//                     {isSignedIn ? (
+//                       <div className="flex justify-end items-center space-x-4">
+//                         <Button asChild>
+//                           <Link href={"/create"}>
+//                             <span>Create a New Trip</span>
+//                             <Plus />
+//                           </Link>
+//                         </Button>
+//                         <UserButton
+//                           appearance={{
+//                             elements: {
+//                               userButtonAvatarBox: {
+//                                 width: "48px",
+//                                 height: "48px",
+//                               },
+//                             },
+//                           }}
+//                         />
+//                       </div>
+//                     ) : (
+//                       <>
+//                         <Button asChild variant="outline">
+//                           <a href={auth.login.url}>{auth.login.title}</a>
+//                         </Button>
+//                         <Button asChild>
+//                           <a href={auth.signup.url}>{auth.signup.title}</a>
+//                         </Button>
+//                       </>
+//                     )}
+//                   </div>
+//                 </div>
+//               </SheetContent>
+//             </Sheet>
+//           </div>
+//         </div>
+//   )
+// }
