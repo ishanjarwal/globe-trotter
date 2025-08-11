367fd71c5426d82f3b8931df07eed5a03b07fc24
ishanjarwal <ishan.professional01@gmail.com>
2025-08-11 11:33:01 +0530

    add bgshape gradient

--- src/components/BgShape.tsx
+++ src/components/BgShape.tsx
@@ -1,7 +1,24 @@
-const BgShape = ({top='auto', left='auto', right='auto',bottom='auto',height='250px',width='250px'}) => {
+const BgShape = ({
+  top = "auto",
+  left = "auto",
+  right = "auto",
+  bottom = "auto",
+  height = "250px",
+  width = "250px",
+}) => {
   return (
-    <div className="blur-3xl aspect-square rounded-full absolute z-[-1] bg-[var(--bg-color)]" style={{height:height,width:width,top:top,bottom:bottom,left:left,right:right}}></div>
-  )
-}
+    <div
+      className="blur-3xl aspect-square rounded-full absolute z-[-1] bg-[#292019]"
+      style={{
+        height: height,
+        width: width,
+        top: top,
+        bottom: bottom,
+        left: left,
+        right: right,
+      }}
+    ></div>
+  );
+};
 
-export default BgShape
+export default BgShape;
